ed76d32c11ff9c80ec6651f99182baa9f1b7326e
30602jatec <141078825+J-B-Bais@users.noreply.github.com>
2024-03-19 07:52:04 +1000

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,520 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "The first european sailor who sighted the island of new guinea?",
+    answer: "Jorge de Meneses ",
+    options: [
+      "Vasco de Gama",
+      "Jorge de Meneses ",
+      "Christopher Columbus",
+      "Ferdinand Magellan"
+    ]
+  },
+    {
+    numb: 2,
+    question: "In what century european sailors discovered the island of new guinea?",
+    answer: "16th",
+    options: [
+      "10th",
+      "12th",
+      "16th",
+      "21st"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Port moresby was named after him?",
+    answer: "Captain John Moresby",
+    options: [
+      "Christopher Columbus",
+      "Ferdinand Magellan",
+      "Jorge de Meneses",
+      "Captain John Moresby"
+    ]
+  },
+    {
+    numb: 4,
+    question: "In 1884 PNG was split into two, which country colonize the north?",
+    answer: "Germany",
+    options: [
+      "Australia",
+      "USA",
+      "Great Britain",
+      "Germany"
+    ]
+  },
+    {
+    numb: 5,
+    question: "In 1884 PNG was split into two, which country colonize the south?",
+    answer: "Great Britain",
+    options: [
+      "Australia",
+      "USA",
+      "Great Britain",
+      "Germany"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "Who was the first PNG’s Prime Minister?",
+    answer: "Michael Thomas Somare",
+    options: [
+      "Julius Chan",
+      "Michael Somare",
+      "Michael Thomas Somare",
+      "John Guise"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "Where is Michael Thomas Somare from?",
+    answer: "East Sepik Province",
+    options: [
+      "Niu Ailan Province",
+      "West Sepik Province",
+      "East Sepik Province",
+      "Morobe Province"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "Who was the first PNG’s Governor General?",
+    answer: "Sir John Guise",
+    options: [
+      "Michae Ogio",
+      "Sam Basil",
+      "Bob Dadae",
+      "Sir John Guise"
+    ]
+  },
+
+  
+  {
+    numb: 9,
+    question: "When did PNG gain her Self-Government?",
+    answer: "1973",
+    options: [
+      "1975",
+      "1970",
+      "1973",
+      "1972"
+    ]
+  },
+
+
+  {
+    numb: 10,
+    question: "When did PNG gain her independence?",
+    answer: "1975",
+    options: [
+      "1973",
+      "1970",
+      "1975",
+      "1972"
+    ]
+  },
+
+
+  {
+    numb: 11,
+    question: "The capital city of PNG?",
+    answer: "Port Moresby",
+    options: [
+      "Hagen",
+      "Lae",
+      "Port Moresby",
+      "NCD"
+    ]
+  },
+
+
+  
+  {
+    numb: 12,
+    question: "The highest peak in PNG?",
+    answer: "Mt. Wilhelm",
+    options: [
+      "Mt. Hagen",
+      "Mt. Wilhelm",
+      "Mt. Kubar",
+      "Star Mountains"
+    ]
+  },
+
+ 
+  {
+    numb: 13,
+    question: "The longest river in PNG?",
+    answer: "Sepik River",
+    options: [
+      "Waghi River",
+      "Markham River",
+      "Sepik River",
+      "Fly River"
+    ]
+  },
+
+  
+
+  {
+    numb: 14,
+    question: "The biggest river in PNG?",
+    answer: "Fly River",
+    options: [
+      "Purari River",
+      "Ramu River",
+      "Sepik River",
+      "Fly River"
+    ]
+  },
+
+  
+  {
+    numb: 15,
+    question: "The first mission agency to come to PNG?",
+    answer: "Catholic",
+    options: [
+      "Methodist",
+      "Catholic",
+      "Anglican",
+      "Four Square"
+    ]
+  },
+
+  
+
+  {
+    numb: 16,
+    question: "Who designed PNG flag?",
+    answer: "Susan Hareho Karike",
+    options: [
+      "Tei Abel",
+      "Paulus Matane",
+      "Susan Karike",
+      "Susan Hareho Karike"
+    ]
+  },
+
+  
+  {
+    numb: 17,
+    question: "Where is Susan Hareho Karike from?",
+    answer: "Gulf",
+    options: [
+      "Central",
+      "NCD",
+      "Gulf",
+      "Western"
+    ]
+  },
+  
+  {
+    numb: 18,
+    question: "What do the abbreviation ANGAU stands for?",
+    answer: "Australian & New Zealand Administrative Unit",
+    options: [
+      "Australian New Guinea Administrative Unit",
+      "Australian & New Zealand Administrative Unit",
+      "Australian New Guinea Admittance Unit",
+      "Australian New Guinea Admittable Unit"
+    ]
+  },
+
+
+   
+  {
+    numb: 19,
+    question: "The second largest city in PNG?",
+    answer: "Lae",
+    options: [
+      "Port Moresby",
+      "Hagen",
+      "Rabaul",
+      "Lae"
+    ]
+  },
+
+
+  {
+    numb: 20,
+    question: "The third largest city in PNG?",
+    answer: "Hagen",
+    options: [
+      "Port Moresby",
+      "Hagen",
+      "Rabaul",
+      "Lae"
+    ]
+  },
+
+
+  {
+    numb: 21,
+    question: "When did PNG Unitech established?",
+    answer: "1970",
+    options: [
+      "1975",
+      "1970",
+      "1960",
+      "1965"
+    ]
+  },
+  
+
+  {
+    numb: 22,
+    question: "When did UPNG established?",
+    answer: "1965",
+    options: [
+      "1975",
+      "1970",
+      "1965",
+      "1960"
+    ]
+  },
+
+  
+  {
+    numb: 23,
+    question: "The most industrial city in PNG?",
+    answer: "Lae",
+    options: [
+      "Port Moresby",
+      "Lae",
+      "Hagen",
+      "Rabaul"
+    ]
+  },
+
+  
+  {
+    numb: 24,
+    question: "Who was the first female member of the parliament in PNG?",
+    answer: "Dame Josephine Abaijah",
+    options: [
+      "Lady Carol Kidu",
+      "Dame Josephine Abaijah",
+      "Hon Kessy Sawang",
+      "Rachael Marape"
+    ]
+  },
+
+  
+  {
+    numb: 25,
+    question: "The first australian high commissioner to PNG?",
+    answer: "Les Johnson",
+    options: [
+      "Tom Critchley",
+      "Les Johnson",
+      "Gerry Nutter",
+      "John Green"
+    ]
+  },
+
+  
+  {
+    numb: 26,
+    question: "The second largest island nation in the world?",
+    answer: "New Guinea",
+    options: [
+      "Madagascar",
+      "Greenland",
+      "New Guinea",
+      "Nertherlands"
+    ]
+  },
+
+
+  
+  {
+    numb: 27,
+    question: "PNG was the host country that hosts a most significant summit in 2018?",
+    answer: "Apec",
+    options: [
+      "World Health Day",
+      "Apec",
+      "World Environment Day",
+      "Asean Meeting"
+    ]
+  },
+
+  {
+    numb: 28,
+    question: "What do the abbreviation APEC stands for?",
+    answer: "Asia Pacific Economic Cooperation",
+    options: [
+      "Asia Pacific Environment Cooperation",
+      "Asia Pacific Economic Cooperation",
+      "Asia Pacific Ecological Cooperation",
+      "Asia Pacific Ecosystem Cooperation"
+    ]
+  },
+
+  {
+    numb: 29,
+    question: "The country that gave PNG her independance?",
+    answer: "Australia",
+    options: [
+      "Australia",
+      "Great Britain",
+      "Germany",
+      "Portugal"
+    ]
+  },
+
+  {
+    numb: 30,
+    question: "PNG's national bird?",
+    answer: "Bird of Paradise",
+    options: [
+      "Turkey",
+      "Cassowary",
+      "Bird of Paradise",
+      "King fisher"
+    ]
+  },
+
+  {
+    numb: 31,
+    question: "Birdwing butterfly is found in?",
+    answer: "Oro",
+    options: [
+      "Milne Bay",
+      "Alotau",
+      "Oro",
+      "Central"
+    ]
+  },
+
+
+  {
+    numb: 32,
+    question: "Tapa cloth is found in?",
+    answer: "Oro",
+    options: [
+      "Milne Bay",
+      "Oro",
+      "Alotau",
+      "Central"
+    ]
+  },
+
+
+  {
+    numb: 33,
+    question: "Hiri trade was practiced in?",
+    answer: "Central",
+    options: [
+      "Hiri",
+      "Central",
+      "Gulf",
+      "Kerema"
+    ]
+  },
+
+  {
+    numb: 34,
+    question: "Kula trade was practiced in?",
+    answer: "Milne Bay",
+    options: [
+      "Hiri",
+      "Central",
+      "Gulf",
+      "Milne Bay"
+    ]
+  },
+
+  {
+    numb: 35,
+    question: "Panguna mine commenced its operation in?",
+    answer: "1972",
+    options: [
+      "1973",
+      "1972",
+      "1999",
+      "1990"
+    ]
+  },
+
+  {
+    numb: 36,
+    question: "Peter O'neill was elected as prime minister in?",
+    answer: "2011",
+    options: [
+      "2009",
+      "2010",
+      "2011",
+      "2012"
+    ]
+  },
+
+
+  {
+    numb: 37,
+    question: "2020-2021 who was our deputy prime minister?",
+    answer: "Sam Basil",
+    options: [
+      "Belden Namah",
+      "Sam Basil",
+      "Brian Kramer",
+      "John Rosso"
+    ]
+  },
+
+  {
+    numb: 38,
+    question: "Who is our current prime minister?",
+    answer: "James Marape",
+    options: [
+      "Belden Namah",
+      "James Marape",
+      "Brian Kramer",
+      "John Rosso"
+    ]
+  },
+
+  {
+    numb: 39,
+    question: "PNG's development state?",
+    answer: "Developing",
+    options: [
+      "Developed",
+      "Improved",
+      "Developing",
+      "Much More Improved"
+    ]
+  },
+
+  {
+    numb: 40,
+    question: "PNG's current speaker?",
+    answer: "Job Pomat",
+    options: [
+      "Sam Basil Junior",
+      "Konny Uguan",
+      "Job Pomat",
+      "Joseph Lelang"
+    ]
+  },
+
+  
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
